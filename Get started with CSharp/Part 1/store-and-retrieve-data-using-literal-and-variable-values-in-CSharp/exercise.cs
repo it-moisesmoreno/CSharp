7af1cd8a25e825ebab71bee6b5cf9c3047fbcbfa
it-moisesmoreno <130410927+it-moisesmoreno@users.noreply.github.com>
2023-06-14 20:40:45 -0600

    Store and retrieve data using literal and variable values in C#
    Added exercises

---
+++ Get started with CSharp/Part 1/store-and-retrieve-data-using-literal-and-variable-values-in-CSharp/exercise.cs
@@ -0,0 +1,20 @@
+string firstName  = "Bob";
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+var message = "Hello world!";
+
+string secondName  = "Bob";
+int messages = 3;
+var temperature = 34.4m;
+Console.Write("Hello, " + secondName + "! You have " + messages + " in your inbox. The tempertature is " + temperature + " celsius.");
